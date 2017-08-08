b120e35d0194bdd5ae5c0f3275bdd412fe9e9125
Lucas Vieira <vieiralucas4@gmail.com>
2017-08-08 03:44:16 -0300

    Can say bitcoin price fetching from blockchain.info

---
+++ index.js
@@ -0,0 +1,34 @@
+const { RtmClient, CLIENT_EVENTS, RTM_EVENTS } = require('@slack/client')
+const axios = require('axios')
+
+const rtm = new RtmClient(process.env.BOT_TOKEN)
+
+let channel
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, ({ channels }) => {
+  channel = channels.find(c => c.is_member && c.name === 'chatbot').id
+})
+
+rtm.on(RTM_EVENTS.MESSAGE, message => {
+  if (message.text.indexOf('bitcoin') > -1) {
+    axios
+      .get('https://blockchain.info/pt/ticker')
+      .then(({ data }) => data)
+      .then(({ BRL }) => {
+        const { symbol, buy, sell } = BRL
+        rtm.sendMessage(
+          `According to blockchain.info: buy: ${symbol}${buy}, sell: ${symbol}${sell}`,
+          channel
+        )
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+})
+
+// you need to wait for the client to fully connect before you can send messages
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {})
+
+rtm.start()
